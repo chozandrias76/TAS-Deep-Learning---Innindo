8a71fb0abd36493a638293e0dd697faf777023d4
Colin Swenson-Healey <chozandrias76@gmail.com>
2018-07-02 21:20:49 -0700

    Trying new system for capture.

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ var Jimp = require("jimp");
 
 var dt = new Date();
 
-import * as tf from '@tensorflow/tfjs';
+// import * as tf from '@tensorflow/tfjs';
 
-// Define a model for linear regression.
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+// // Define a model for linear regression.
+// const model = tf.sequential();
+// model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
-// Prepare the model for training: Specify the loss and the optimizer.
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+// // Prepare the model for training: Specify the loss and the optimizer.
+// model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+// // Generate some synthetic data for training.
+// const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+// const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
 
-// Train the model using the data.
-model.fit(xs, ys, {epochs: 10}).then(() => {
-  // Use the model to do inference on a data point the model hasn't seen before:
-  model.predict(tf.tensor2d([5], [1, 1])).print();
-});
+// // Train the model using the data.
+// model.fit(xs, ys, {epochs: 10}).then(() => {
+//   // Use the model to do inference on a data point the model hasn't seen before:
+//   model.predict(tf.tensor2d([5], [1, 1])).print();
+// });
 
 // Speed up the mouse.
 // robot.setMouseDelay(2);
@@ -89,23 +89,31 @@ model.fit(xs, ys, {epochs: 10}).then(() => {
 // }
 // getVersion();
 
-// const fullDate = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + "/";
-// for(var i = (dt.getSeconds()); i < (dt.getSeconds()+10); i++){
-//   var start = (new Date().getTime());
-//   var screen = robot.screen.capture(0,0,1920,1080);
-// 	var image = new Jimp(screen.width, screen.height, function(err, img) {
-// 	img.bitmap.data = screen.image;
-// 	img.scan(0, 0, img.bitmap.width, img.bitmap.height, function (x, y, idx) {
-// 		var red   = img.bitmap.data[ idx + 0 ];
-// 		var blue  = img.bitmap.data[ idx + 2 ];
-// 		img.bitmap.data[ idx + 0 ] = blue;
-// 		img.bitmap.data[ idx + 2 ] = red;
+const fullDate = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + "/";
+// for(var i = (dt.getSeconds()); i < (dt.getSeconds()+10); i++)
+let promises = []
+var i = 0;
+var start = (new Date().getTime());
+while(i % 1001 != 1000){
+  var screen = robot.screen.capture(0,0,1920,1080);
+	var image = new Jimp(screen.width, screen.height, function(err, img) {
+	img.bitmap.data = screen.image;
+	img.scan(0, 0, img.bitmap.width, img.bitmap.height, function (x, y, idx) {
+		var red   = img.bitmap.data[ idx + 0 ];
+		var blue  = img.bitmap.data[ idx + 2 ];
+		img.bitmap.data[ idx + 0 ] = blue;
+		img.bitmap.data[ idx + 2 ] = red;
 		
-//   });
-//   console.log((new Date().getTime()-start)/60);
-// 	if(i & 1){
-// 		img.write("./images/" + fullDate + "/ right monitor " + new Date().getTime() + '.png');
-// 	}
-	
-// 	});
-// }
+  });
+  i++;
+  console.log(i);
+	// if(i & 1){
+    promises.push(new Promise((r, j) => {
+      img.write("./images/" + fullDate + "/ right monitor " + new Date().getTime() + '.png');
+    }));
+  // }
+	});
+}
+Promise.all(promises).then(() => {
+  console.log("done")
+});
---
+++ node-castro/README.md
@@ -0,0 +1,77 @@
+Castro
+===========
+
+Screen recording library for Node
+
+## Awesome features!
+
+- Uses the [AV Foundation framework](https://developer.apple.com/av-foundation/) to record the highest-quality video you possibly could record. 
+
+- Records in the popular MPEG-4 file format.
+
+- Simple API. Start and stop -- that's all you need to know!
+
+- Automation friendly. Combine with test automation tools like [Appium](http://appium.io/) or [Selenium](http://docs.seleniumhq.org/) to record movies of your tests. Screen recordings can be useful for debugging test failures.  
+
+- Open source. (MIT License)
+
+
+## Install
+
+    npm install castro
+
+## Example usage
+
+    castro = require('castro')
+    movie = new castro.Castro()
+    movie.start()
+    // Do something awesome
+    movie.stop()
+
+## Where's my movie?
+
+Movies are stored on the Desktop...
+   
+    > movie.location
+    '/Users/hugs/Desktop/Castro_5wmi.mp4'
+    
+    In other words:
+    '<home>/Desktop/Castro_<short_random_string>.mp4'
+
+... but you can change the location before you start recording.
+
+    > movie = new castro.Castro()
+    > movie.setLocation('/Users/me/epic-movies/totes-epic.mp4')
+    > movie.start()
+
+## Requirements
+
+Castro was tested with:
+
+    $ node -v
+    v4.5.0
+
+    $ sw_vers
+    ProductName:	Mac OS X
+    ProductVersion:	10.11.6
+    BuildVersion:	15G1004
+
+*Only works on OS X for now.*
+
+## Example movies recorded with Castro
+  
+[![Castro](https://s3.amazonaws.com/castro-screencasts/hi-castro-medium.png "hi, castro")](http://youtu.be/0qdPCl4TFt8)
+  
+- Movie recorded with Castro
+
+  + [Original (mp4, 3.9 MB)](https://s3.amazonaws.com/castro-screencasts/Castro_h41jor.mp4) 
+  + [YouTube](http://youtu.be/0qdPCl4TFt8)
+
+- Using Castro to make the above movie
+
+  + [Original (mp4, 14.4 MB)](https://s3.amazonaws.com/castro-screencasts/Castro_8jv2t9.mp4)
+  + [YouTube](http://youtu.be/PAYoa9fglMk) 
+  
+  (Yes, you can have two Castro movies recording at the same time!)
+
+
